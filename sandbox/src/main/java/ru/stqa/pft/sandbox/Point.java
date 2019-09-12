867d25b29dafe89ed94d53919c5cd4a6ac48011d
2299350 <55190254+2299350@users.noreply.github.com>
2019-09-12 15:49:47 +0300

    Update Point.java

--- sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
@@ -1,12 +1,15 @@
 package ru.stqa.pft.sandbox;
 
 public class Point {
+
   double x;
   double y;
 
   public Point(double x, double y) {
+
     this.x = x;
     this.y = y;
+
   }
 
   public double distance(Point anotherPoint) {
