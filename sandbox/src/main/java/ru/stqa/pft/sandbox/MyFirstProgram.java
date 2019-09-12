897bf89a182174388ae374a046469fe8a63cbf3e
2299350 <55190254+2299350@users.noreply.github.com>
2019-09-12 14:38:41 +0300

    Task 2. The first implementation (in the main class)

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -3,28 +3,15 @@ package ru.stqa.pft.sandbox;
 public class MyFirstProgram {
 	
 	public static void main(String[] args) {
-		hello("world");
-		hello("user");
-		hello("Aleksei");
-		System.out.println();
-
-		Square s = new Square(5);
-		System.out.println("The square area with the side " + s.l + " = " + area(s));
-
-		Rectangle r = new Rectangle(4, 6);
-		System.out.println("The square area with the sides " + r.a + " and " + r.b + " = " + area(r));
+		Point p1 = new Point(-2.3, 4);
+		Point p2 = new Point(8.5, 0.7);
+		System.out.println("The distance between the points is: " + distance(p1, p2));
 	}
 
-	public static void hello(String somebody) {
-		System.out.println("Hello, " + somebody + "!");
-	}
-
-	public static double area(Square s) {
-		return s.l * s.l;
-	}
-
-	public static double area(Rectangle r) {
-		return r.a * r.b;
+	public static double distance(Point p1, Point p2) {
+		double result;
+		result = Math.sqrt((Math.pow((p2.x - p1.x), 2) + (Math.pow((p2.y - p1.y), 2))));
+		return result;
 	}
 	
 }
