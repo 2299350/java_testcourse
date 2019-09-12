27d64310f590b1e3f25bb78523bd5bd31a746f47
2299350 <55190254+2299350@users.noreply.github.com>
2019-09-12 11:56:48 +0300

    1.5 Types of variables

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -3,10 +3,11 @@ package ru.stqa.pft.sandbox;
 public class MyFirstProgram {
 	
 	public static void main(String[] args) {
-		System.out.println("Hello, world!");
+		String somebody = "world!";
+		System.out.println("Hello, " + somebody);
 
-		int l = 6;
-		int s = l * l;
+		double l = 8.0;
+		double s = l * l;
 		System.out.println("The square area with the side " + l + " = " + s);
 	}
 	
