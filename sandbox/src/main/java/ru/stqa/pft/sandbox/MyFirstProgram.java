db2071eba84448f2dfea060698cbd207c88465cc
2299350 <55190254+2299350@users.noreply.github.com>
2019-09-12 15:49:44 +0300

    Formating

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -6,6 +6,7 @@ public class MyFirstProgram {
 		Point p1 = new Point(-2.3, 4);
 		Point p2 = new Point(8.5, 0.7);
 		System.out.println("The distance between the points is: " + p1.distance(p2));
+		
 	}
 
 }
