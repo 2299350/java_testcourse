f59ffc5a8675b912f74408beb917937729150c67
2299350 <55190254+2299350@users.noreply.github.com>
2019-09-12 16:13:35 +0300

    2 tests passed, 1 failed

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -3,10 +3,10 @@ package ru.stqa.pft.sandbox;
 public class MyFirstProgram {
 	
 	public static void main(String[] args) {
-		Point p1 = new Point(-2.3, 4);
-		Point p2 = new Point(8.5, 0.7);
+		Point p1 = new Point(7, 5.5);
+		Point p2 = new Point(2, 6);
 		System.out.println("The distance between the points is: " + p1.distance(p2));
-		
+
 	}
 
 }
---
+++ sandbox/src/test/java/ru/stqa/pft/sandbox/PointDistanceTest.java
@@ -0,0 +1,47 @@
+package ru.stqa.pft.sandbox;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class PointDistanceTest {
+
+    @Test
+    public void testDistance() {
+
+      Point p1 = new Point(2.0, 3.0);
+      Point p2 = new Point(3.0, 4.0);
+      double result = p1.distance(p2);
+
+      Assert.assertEquals(result, 1.4142135623730951);
+
+    }
+
+   @Test
+   public void testDistance2() {
+
+     Point p1 = new Point(7, 5.5);
+     Point p2 = new Point(2, 6);
+     double result = p1.distance(p2);
+
+     Assert.assertEquals(result, 5.024937810560445);
+
+   }
+
+  @Test
+  public void testDistance3() {
+
+    Point p1 = new Point(8, 5.5);
+    Point p2 = new Point(4, 6);
+    double result = p1.distance(p2);
+
+    Assert.assertEquals(result, 0.0);
+
+  }
+
+}
+
+
+
+
+
+
