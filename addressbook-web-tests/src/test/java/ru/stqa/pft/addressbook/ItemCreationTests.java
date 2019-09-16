4e4bad34336a87a0df181e4baaae81f9e30c7676
2299350 <55190254+2299350@users.noreply.github.com>
2019-09-16 12:13:43 +0300

    ItemCreationTests and ItemData have been added

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/ItemCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/ItemCreationTests.java
@@ -6,14 +6,13 @@ import org.testng.annotations.*;
 import org.openqa.selenium.*;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
-public class UntitledTestCase {
+public class ItemCreationTests {
+
   private WebDriver wd;
-  private String baseUrl;
-  private boolean acceptNextAlert = true;
-  private StringBuffer verificationErrors = new StringBuffer();
 
   @BeforeMethod(alwaysRun = true)
   public void setUp() throws Exception {
+
     wd = new FirefoxDriver();
     wd.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
 
@@ -32,25 +31,45 @@ public class UntitledTestCase {
   }
 
   @Test
-  public void testUntitledTestCase() throws Exception {
+  public void testGroupCreation() throws Exception {
+
+    gotoAddPage("add new");
+    ItemData itemData = new ItemData("firstname", "middlename", "lastname", "home", "mobile");
+    fillGroupForm(itemData);
+    submitItemCreation();
+    returnToMainPage();
+  }
+
+  private void gotoAddPage(String page) {
+
+    wd.findElement(By.linkText(page)).click();
+  }
+
+  private void fillGroupForm(ItemData itemData) {
+
+    wd.findElement(By.name(itemData.getFirstname())).click();
+    wd.findElement(By.name(itemData.getFirstname())).clear();
+    wd.findElement(By.name(itemData.getFirstname())).sendKeys("FN");
+    wd.findElement(By.name(itemData.getMiddlename())).click();
+    wd.findElement(By.name(itemData.getMiddlename())).clear();
+    wd.findElement(By.name(itemData.getMiddlename())).sendKeys("MN");
+    wd.findElement(By.name(itemData.getLastname())).click();
+    wd.findElement(By.name(itemData.getLastname())).clear();
+    wd.findElement(By.name(itemData.getLastname())).sendKeys("LN");
+    wd.findElement(By.name(itemData.getHome())).click();
+    wd.findElement(By.name(itemData.getHome())).clear();
+    wd.findElement(By.name(itemData.getHome())).sendKeys("+79502233650");
+    wd.findElement(By.name(itemData.getMobile())).click();
+    wd.findElement(By.name(itemData.getMobile())).clear();
+    wd.findElement(By.name(itemData.getMobile())).sendKeys("+79502233652");
+  }
+
+  private void returnToMainPage() {
     wd.get("http://localhost:8080/");
-    wd.findElement(By.linkText("add new")).click();
-    wd.findElement(By.name("firstname")).click();
-    wd.findElement(By.name("firstname")).clear();
-    wd.findElement(By.name("firstname")).sendKeys("FN");
-    wd.findElement(By.name("middlename")).click();
-    wd.findElement(By.name("middlename")).clear();
-    wd.findElement(By.name("middlename")).sendKeys("MN");
-    wd.findElement(By.name("lastname")).click();
-    wd.findElement(By.name("lastname")).clear();
-    wd.findElement(By.name("lastname")).sendKeys("LN");
-    wd.findElement(By.name("home")).click();
-    wd.findElement(By.xpath("//body")).click();
-    wd.findElement(By.name("home")).clear();
-    wd.findElement(By.name("home")).sendKeys("+79502233650");
-    wd.findElement(By.name("mobile")).click();
-    wd.findElement(By.name("mobile")).clear();
-    wd.findElement(By.name("mobile")).sendKeys("+79502233652");
+  }
+
+  public void submitItemCreation() {
+
     wd.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='Notes:'])[1]/following::input[1]")).click();
   }
 
@@ -61,6 +80,7 @@ public class UntitledTestCase {
   }
 
   private boolean isElementPresent(By by) {
+
     try {
       wd.findElement(by);
       return true;
@@ -70,26 +90,12 @@ public class UntitledTestCase {
   }
 
   private boolean isAlertPresent() {
+
     try {
       wd.switchTo().alert();
       return true;
     } catch (NoAlertPresentException e) {
       return false;
-    }
-  }
-
-  private String closeAlertAndGetItsText() {
-    try {
-      Alert alert = wd.switchTo().alert();
-      String alertText = alert.getText();
-      if (acceptNextAlert) {
-        alert.accept();
-      } else {
-        alert.dismiss();
-      }
-      return alertText;
-    } finally {
-      acceptNextAlert = true;
     }
   }
 }
---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/ItemData.java
@@ -0,0 +1,39 @@
+package ru.stqa.pft.addressbook;
+
+public class ItemData {
+
+  private final String firstname;
+  private final String middlename;
+  private final String lastname;
+  private final String home;
+  private final String mobile;
+
+  public ItemData(String firstname, String middlename, String lastname, String home, String mobile) {
+
+    this.firstname = firstname;
+    this.middlename = middlename;
+    this.lastname = lastname;
+    this.home = lastname;
+    this.mobile = lastname;
+  }
+
+  public String getFirstname() {
+    return firstname;
+  }
+
+  public String getMiddlename() {
+    return middlename;
+  }
+
+  public String getLastname() {
+    return lastname;
+  }
+
+  public String getHome() {
+    return home;
+  }
+
+  public String getMobile() {
+    return mobile;
+  }
+}
