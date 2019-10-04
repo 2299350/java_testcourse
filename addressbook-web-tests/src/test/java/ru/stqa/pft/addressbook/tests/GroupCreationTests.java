aaf6299e4e382823abb733eec004a7d7d6ac97a2
2299350 <55190254+2299350@users.noreply.github.com>
2019-10-04 17:46:31 +0300

    Lesson 4.9. Lambda expressions (anonymous functions) and elements of functional programming

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
@@ -4,6 +4,7 @@ import org.testng.Assert;
 import org.testng.annotations.*;
 import ru.stqa.pft.addressbook.model.GroupData;
 
+import java.util.Comparator;
 import java.util.HashSet;
 import java.util.List;
 
@@ -16,7 +17,7 @@ public class GroupCreationTests extends TestBase{
     List<GroupData> before = app.getGroupHelper().getGroupList();
 
     //int before = app.getGroupHelper().getGroupCount(); // number of groups before adding
-    GroupData group = new GroupData("Test2", "Header2", "Footer2");
+    GroupData group = new GroupData("TestLambda", "Header2", "Footer2");
     app.getGroupHelper().createGroup(group);
     //int after = app.getGroupHelper().getGroupCount(); // number of groups after adding
     List<GroupData> after = app.getGroupHelper().getGroupList();
@@ -30,6 +31,18 @@ public class GroupCreationTests extends TestBase{
         max = g.getId();
       }
     }
+
+/*    Comparator<? super GroupData> byId = new Comparator<GroupData>() {
+      @Override
+      public int compare(GroupData o1, GroupData o2) {
+        return Integer.compare(o1.getId(), o2.getId());
+      }
+    };*/
+
+    Comparator<? super GroupData> byId = (Comparator<GroupData>) (o1, o2) -> Integer.compare(o1.getId(), o2.getId());
+
+    after.stream().max(byId).get().getId();
+
     group.setId(max); // giving the max id to the group
     before.add(group);
 
