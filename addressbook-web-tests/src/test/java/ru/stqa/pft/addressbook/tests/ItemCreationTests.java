014531229e5c34bfdfd303f3868b42a5816e6b10
2299350 <55190254+2299350@users.noreply.github.com>
2019-10-22 13:56:41 +0300

    Task #13: Parameterize tests to create groups and contacts

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ItemCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ItemCreationTests.java
@@ -1,26 +1,62 @@
 package ru.stqa.pft.addressbook.tests;
 
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+import com.thoughtworks.xstream.XStream;
 import org.testng.annotations.*;
 import ru.stqa.pft.addressbook.model.ItemData;
 import ru.stqa.pft.addressbook.model.Items;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Iterator;
+import java.util.List;
 import java.util.Set;
+import java.util.stream.Collectors;
+
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 public class ItemCreationTests extends TestBase{
 
-  @Test
-  public void testItemCreation() throws Exception {
+  @DataProvider
+  public Iterator<Object[]> validItemsFromXml() throws IOException {
+    BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/items.xml")));
+    String xml = "";
+    String line = reader.readLine();
+
+    while (line != null) {
+      xml += line;
+      line = reader.readLine();
+    }
+    XStream xStream = new XStream();
+    xStream.processAnnotations(ItemData.class);
+    List<ItemData> items = (List<ItemData>) xStream.fromXML(xml);
+    return items.stream().map((i) -> new Object[]{i}).collect(Collectors.toList()).iterator();
+  }
+
+  @DataProvider
+  public Iterator<Object[]> validItemsFromJson() throws IOException {
+    BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/items.json")));
+    String json = "";
+    String line = reader.readLine();
+
+    while (line != null) {
+      json += line;
+      line = reader.readLine();
+    }
+    Gson gson = new Gson();
+    List<ItemData> items = gson.fromJson(json, new TypeToken<List<ItemData>>(){}.getType()); // List<GroupData>.class
+    return items.stream().map((g) -> new Object[]{g}).collect(Collectors.toList()).iterator();
+  }
+
+  @Test (dataProvider = "validItemsFromJson")
+  public void testItemCreation(ItemData item) throws Exception {
 
     Items before = app.item().all();
-
-    File photo = new File("src/test/resources/stru.jpg");
-    ItemData item = new ItemData()
-            .withFName("FName3").withMName("MName3").withLName("LName3")
-            .withHome("+7(920)3696563").withMobile("+7-920-369-65-65").withWork("+7 920 369 65 67").withPhoto(photo);
-
+    //File photo = new File("src/test/resources/stru.jpg");
     app.item().create(item);
 
     Set<ItemData> after = app.item().all();
