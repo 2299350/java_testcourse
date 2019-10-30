ad8c5ba374a3ba305f89ce6dd5b0a121306f942d
2299350 <55190254+2299350@users.noreply.github.com>
2019-10-30 13:52:41 +0300

    Lesson 7.5. Disableable UI Checks

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/TestBase.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/TestBase.java
@@ -5,14 +5,22 @@ import org.slf4j.LoggerFactory;
 import org.testng.annotations.*;
 import ru.stqa.pft.addressbook.appmanager.ApplicationManager;
 import org.openqa.selenium.remote.BrowserType;
+import ru.stqa.pft.addressbook.model.GroupData;
+import ru.stqa.pft.addressbook.model.Groups;
 
 import java.lang.reflect.Method;
 import java.util.Arrays;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+
+import static org.hamcrest.CoreMatchers.*;
+import static org.hamcrest.MatcherAssert.assertThat;
 
 public class TestBase {
   Logger logger = LoggerFactory.getLogger(TestBase.class);
 
   protected static final ApplicationManager app = new ApplicationManager(System.getProperty("browser", BrowserType.CHROME));
+
   // add  -Dbrowser=firefox to Run/Debug Configuration to convey browser type | BrowserType.CHROME will be used by default
   @BeforeSuite
   public void setUp() throws Exception {
@@ -24,13 +32,23 @@ public class TestBase {
     app.stop();
   }
 
-  @BeforeMethod (alwaysRun = true)
+  @BeforeMethod(alwaysRun = true)
   public void logTestStart(Method m, Object[] p) {
     logger.info("Start test " + m.getName() + " with parameters " + Arrays.asList(p));
   }
 
-  @AfterMethod (alwaysRun = true)
+  @AfterMethod(alwaysRun = true)
   public void logTestStop(Method m, Object[] p) {
     logger.info("Stop test " + m.getName() + " with parameters " + Arrays.asList(p));
+  }
+
+  public void verifyGroupListInUI() {
+    if (Boolean.getBoolean("verifyUI")) { //Edit configuration VM options -DverifyUI=true
+      Groups dbGroups = app.db().groups();
+      Groups uiGroups = app.group().all();
+      assertThat(uiGroups, equalTo(dbGroups.stream().map((g) -> new GroupData()
+              .withId(g.getId()).withName(g.getName()))
+              .collect(Collectors.toSet())));
+    }
   }
 }
