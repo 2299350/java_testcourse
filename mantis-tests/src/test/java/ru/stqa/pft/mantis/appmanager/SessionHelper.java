5dd6158ce583f43b156d4f952f035a432980670d
2299350 <55190254+2299350@users.noreply.github.com>
2019-11-20 13:27:37 +0300

    Task #18: Implement a test to change the password in MantisBT

--- mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/SessionHelper.java
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/SessionHelper.java
@@ -1,6 +1,7 @@
 package ru.stqa.pft.mantis.appmanager;
 
 import org.openqa.selenium.By;
+import java.io.IOException;
 
 public class SessionHelper extends HelperBase {
   private ApplicationManager app;
@@ -24,7 +25,7 @@ public class SessionHelper extends HelperBase {
     click(By.cssSelector("input[type='submit']"));
   }
 
-  public void chooseUser (String userLogin) {
+  public String chooseUserAndGetEmail(String userLogin) {
     String userEmail;
     String UsersPage = app.getProperty("web.baseUrl") + "manage_user_page.php";
     wd.get(UsersPage);
@@ -33,5 +34,34 @@ public class SessionHelper extends HelperBase {
 
     userEmail = wd.findElement(By.cssSelector("input[id='email-field']")).getAttribute("value");
     System.out.println(userEmail);
+    click(By.cssSelector("input[value='Reset Password']"));
+
+    return userEmail;
+  }
+
+  public void changePass(String confirmationLink, String password) {
+    app.getDriver().get(confirmationLink);
+    type(By.name("password"), password);
+    type(By.name("password_confirm"), password);
+    click(By.cssSelector("button[type='submit']"));
+  }
+
+  public void loginUserUI (String userLogin, String userPass) {
+    String loginPage = app.getProperty("web.baseUrl") + "login_page.php";
+
+    wd.get(loginPage); //http://localhost:8080/mantisbt-2.22.1/login_page.php
+    type(By.cssSelector("input[id='username']"), userLogin);
+    click(By.cssSelector("input[type='submit']"));
+
+    type(By.cssSelector("input[id='password']"), userPass);
+    click(By.cssSelector("input[type='submit']"));
+  }
+  public boolean isLoggedInAs(String username) throws IOException {
+    wd.get(app.getProperty("web.baseUrl") + "/index.php");
+    String xpath = String.format("//span[@class='user-info' and contains(text(), '%s')]", username);
+    if (!wd.findElement(By.xpath(xpath)).getText().equals(username)) {
+      return false;
+    }
+    return true;
   }
 }
--- mantis-tests/src/test/java/ru/stqa/pft/mantis/tests/ChangePasswordTests.java
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/tests/ChangePasswordTests.java
@@ -1,10 +1,21 @@
 package ru.stqa.pft.mantis.tests;
 
+import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
+import ru.lanwen.verbalregex.VerbalExpression;
+import ru.stqa.pft.mantis.model.MailMessage;
+
+import javax.mail.MessagingException;
+import java.io.IOException;
+import java.util.List;
 
 public class ChangePasswordTests extends TestBase {
+
+  private String userName;
+  private String userEmail;
+  private String newPassword;
 
   @BeforeMethod
   public void startMailServer() {
@@ -12,21 +23,26 @@ public class ChangePasswordTests extends TestBase {
   }
 
   @Test
-  public void testChangePassword() {
-    String userName = "user13";
+  public void testChangePassword() throws IOException, MessagingException {
+    userName = "user13";
+    newPassword = "newPassword";
+
     app.session().loginAdminUI(); // Login as an admin
-    app.session().chooseUser(userName);
+    userEmail = app.session().chooseUserAndGetEmail(userName); // Init password change and get user's email
 
-    /*>> Логинимся под админом
-      >> Заходим на http://localhost:8080/mantisbt-2.22.1/manage_user_page.php
-      >> Выбрать пользователя по username и кликнуть на него
-      >> На открывшейся странице нажать кнопку <input type="submit" class="btn btn-primary btn-white btn-round" value="Сбросить пароль">
+    List<MailMessage> mailMessages = app.mail().waitForMail(1,10000); // Get confirmation link
+    String confirmationLink = findConfirmationLink(mailMessages, userEmail);
 
-      >> Получить письмо
-      >> Извлечь из него ссылку для смены пароля
-      >> Пройти по этой ссылке и изменить пароль
+    app.session().changePass(confirmationLink, newPassword); // Change the pass
 
-      >> Залогиниться под новыми учетными данными*/
+    app.session().loginUserUI(userName, newPassword); // Login with a new pass
+    Assert.assertTrue(app.session().isLoggedInAs(userName)); // Check login with new credentials
+  }
+
+  private String findConfirmationLink(List<MailMessage> mailMessages, String email) {
+    MailMessage mailMessage = mailMessages.stream().filter((m) -> m.to.equals(email)).findFirst().get();
+    VerbalExpression regex = VerbalExpression.regex().find("http://").nonSpace().oneOrMore().build();
+    return regex.getText(mailMessage.text);
   }
 
   @AfterMethod(alwaysRun = true)
