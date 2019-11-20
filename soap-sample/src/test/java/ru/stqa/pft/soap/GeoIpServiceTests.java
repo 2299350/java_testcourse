68f457622cd73c3a324de1f1f35ce750f223f315
2299350 <55190254+2299350@users.noreply.github.com>
2019-11-20 18:43:48 +0300

    Lesson 9.2. SOAP: A Simple Example (http://wsgeoip.lavasoft.com/ipservice.asmx)

---
+++ soap-sample/src/test/java/ru/stqa/pft/soap/GeoIpServiceTests.java
@@ -0,0 +1,15 @@
+package ru.stqa.pft.soap;
+
+import com.lavasoft.GeoIPService;
+import com.lavasoft.GetCountryISO2ByName;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class GeoIpServiceTests {
+
+  @Test
+  public void testMyIP() {
+    String geoIP = new GeoIPService().getGeoIPServiceSoap12().getIpLocation("194.28.29.152");
+    Assert.assertEquals(geoIP, "<GeoIP><Country>RU</Country><State>47</State></GeoIP>");
+  }
+}
